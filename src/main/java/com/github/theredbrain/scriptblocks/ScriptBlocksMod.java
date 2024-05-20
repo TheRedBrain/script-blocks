39170b7e04ace8b06b38468966d12b53657f0428
TheRedBrain <thetrueredbrain@gmail.com>
2024-05-20 15:24:14 +0200

    clean up

--- src/main/java/com/github/theredbrain/scriptblocks/ScriptBlocksMod.java
+++ src/main/java/com/github/theredbrain/scriptblocks/ScriptBlocksMod.java
@@ -14,9 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ScriptBlocksMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final String MOD_ID = "scriptblocks";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static ServerConfig serverConfig;
@@ -35,11 +32,7 @@ public class ScriptBlocksMod implements ModInitializer {
 		// Registry
 		BlockRegistry.init();
 		EntityRegistry.init();
-//		EntityAttributesRegistry.registerAttributes();
-//		EntityAttributesRegistry.registerEntityAttributes();
-//		DataHandlerRegistry.init();
 		DimensionsManager.init();
-//		CraftingRecipeRegistry.init();
 		EventsRegistry.initializeEvents();
 		DialoguesRegistry.init();
 		DialogueAnswersRegistry.init();
@@ -50,7 +43,6 @@ public class ScriptBlocksMod implements ModInitializer {
 		ScreenHandlerTypesRegistry.registerAll();
 		StatusEffectsRegistry.registerEffects();
 		GameRulesRegistry.init();
-//		PredicateRegistry.init();
 		StructurePlacementTypesRegistry.register();
 	}
 
