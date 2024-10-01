6a1a4d15d77c134434d275f47bd26b685b41372d
TheRedBrain <thetrueredbrain@gmail.com>
2024-10-01 17:08:36 +0200

    update to 1.21.1
    updated dependencies

--- src/testmod/java/com/github/theredbrain/scriptblocks_test/ScriptBlocksTest.java
+++ src/testmod/java/com/github/theredbrain/scriptblocks_test/ScriptBlocksTest.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ScriptBlocksTest implements ModInitializer {
-	public static final String MOD_ID = "scriptblocks";
+	public static final String MOD_ID = "scriptblocks_test";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
