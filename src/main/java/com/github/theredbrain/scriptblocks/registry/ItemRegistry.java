7d8986fd70603d16f4e4083cec33f3785c1429d6
TheRedBrain <thetrueredbrain@gmail.com>
2024-10-20 13:57:05 +0200

    fixed BlockPositionDistanceMeterItem not working

--- src/main/java/com/github/theredbrain/scriptblocks/registry/ItemRegistry.java
+++ src/main/java/com/github/theredbrain/scriptblocks/registry/ItemRegistry.java
@@ -1,6 +1,7 @@
 package com.github.theredbrain.scriptblocks.registry;
 
 import com.github.theredbrain.scriptblocks.ScriptBlocks;
+import com.github.theredbrain.scriptblocks.component.type.BlockPositionDistanceMeterComponent;
 import com.github.theredbrain.scriptblocks.item.BlockPositionDistanceMeterItem;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
@@ -12,7 +13,7 @@ import org.jetbrains.annotations.Nullable;
 
 public class ItemRegistry {
 
-	public static final Item BLOCK_POSITION_DISTANCE_METER = registerItem("block_position_distance_meter", new BlockPositionDistanceMeterItem(new Item.Settings().maxCount(1)), ItemGroupRegistry.SCRIPT_BLOCKS);
+	public static final Item BLOCK_POSITION_DISTANCE_METER = registerItem("block_position_distance_meter", new BlockPositionDistanceMeterItem(new Item.Settings().maxCount(1).component(ItemComponentRegistry.BLOCK_POSITION_DISTANCE_METER, BlockPositionDistanceMeterComponent.DEFAULT)), ItemGroupRegistry.SCRIPT_BLOCKS);
 
 	private static Item registerItem(String name, Item item, @Nullable RegistryKey<ItemGroup> itemGroup) {
 
