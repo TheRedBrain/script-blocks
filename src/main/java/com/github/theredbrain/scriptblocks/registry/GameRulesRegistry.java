9ae3ea457d00f341e8937eabda1efa8d06ba93cd
TheRedBrain <thetrueredbrain@gmail.com>
2024-11-12 23:07:30 +0100

    added missing assets
    - commented out unused gamerule

--- src/main/java/com/github/theredbrain/scriptblocks/registry/GameRulesRegistry.java
+++ src/main/java/com/github/theredbrain/scriptblocks/registry/GameRulesRegistry.java
@@ -5,8 +5,8 @@ import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.minecraft.world.GameRules;
 
 public class GameRulesRegistry {
-	public static final GameRules.Key<GameRules.BooleanRule> SCRIPT_BLOCKS_DROP_THEMSELF =
-			GameRuleRegistry.register("scriptBlocksDropThemself", GameRules.Category.DROPS, GameRuleFactory.createBooleanRule(true));
+//	public static final GameRules.Key<GameRules.BooleanRule> SCRIPT_BLOCKS_DROP_THEMSELF =
+//			GameRuleRegistry.register("scriptBlocksDropThemself", GameRules.Category.DROPS, GameRuleFactory.createBooleanRule(true));
 	public static final GameRules.Key<GameRules.BooleanRule> MIMIC_DEBUG_MODE =
 			GameRuleRegistry.register("mimicDebugMode", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
 
