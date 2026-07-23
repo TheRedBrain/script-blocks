dbe74481080b4c639c500cd3a8e992c6084f297a
TheRedBrain <thetrueredbrain@gmail.com>
2026-07-23 20:27:02 +0200

    potential fix for DataRelayBlock issue

--- src/main/java/com/github/theredbrain/scriptblocks/network/packet/UpdateDataRelayBlockPacketReceiver.java
+++ src/main/java/com/github/theredbrain/scriptblocks/network/packet/UpdateDataRelayBlockPacketReceiver.java
@@ -32,7 +32,7 @@ public class UpdateDataRelayBlockPacketReceiver implements ServerPlayNetworking.
 		BlockState blockState = world.getBlockState(dataRelayBlockPosition);
 
 		if (blockEntity instanceof DataRelayBlockEntity dataRelayBlockEntity) {
-			dataRelayBlockEntity.reset();
+//			dataRelayBlockEntity.reset(); // TODO test if the coordinate sign flipping stops
 			dataRelayBlockEntity.setDataProvidingBlockPosOffsetList(dataProvidingBlockPosOffsetList);
 			dataRelayBlockEntity.setIndex(0);
 			serverPlayerEntity.sendMessage(Text.translatable("hud.message.script_block.update_successful"), true);
